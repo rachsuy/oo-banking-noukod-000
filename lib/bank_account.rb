98d5f8988efe4f711b3a18b6916c8bf7fb9259e1
Rachele Suy <rachelesuy@gmail.com>
2019-05-22 09:08:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -14,8 +14,17 @@ class BankAccount
   end
   
   def display_balance
-    puts "Your balance is $#{@balance}"
+    "Your balance is $#{@balance}."
   end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
   
 
 end
