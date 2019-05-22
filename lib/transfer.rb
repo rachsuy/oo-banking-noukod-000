29e2fda85c909db9265fb8d2311a5e4f2e2f1b78
Rachele Suy <rachelesuy@gmail.com>
2019-05-22 09:12:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,25 @@ class Transfer
   end
   
   def valid?
-    balance > 0 && status =
+    @sender.valid? && @receiver.valid? ? true : false
   end
 
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
